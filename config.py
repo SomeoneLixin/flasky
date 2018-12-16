24e7dbaf6ad12f050b1263b101e2f5824049c852
wanglixin <1304382389@qq.com>
2018-12-16 12:27:58 +0800

    three commit

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     @staticmethod
     def init_app(app):
-    pass
+        pass
 
 class DevelopmentConfig(Config):
     DEBUG = True
@@ -34,7 +34,7 @@ config = {
     'development': DevelopmentConfig,
     'testing': TestingConfig,
     'production': ProductionConfig,
-        
+
     'default': DevelopmentConfig
 }
 
--- hello.py
+++ hello.py
@@ -1,54 +1,103 @@
-from flask import Flask
-from flask import request, make_response, redirect, abort, url_for, render_template
-from flask_script import Manager
-import config
+# from flask import Flask
+# from flask import request, make_response, redirect, abort, url_for, render_template
+# from flask_script import Manager
+# import config
+#
+#
+# app = Flask(__name__)
+# app.config.from_object(config)
+# manager = Manager(app)
+#
+# @app.route('/')
+# def index():
+#     # user_agent = request.headers.get('User-Agent')
+#     # return '<p>Your Browser is %s</p>' % user_agent
+#     # response = make_response('<h1>This document carries a cookie!</h1>')
+#     # response.set_cookie('answer', '42')
+#     # return response
+#     # print (url_for('get_user', id='1'))
+#     # print (url_for('user', name='wanglixin'))
+#     # return 'Hello world!'
+#     # login_url = url_for('login')
+#     # return redirect(login_url)
+#     context = {
+#         'username' : '知了课堂',
+#         'gender' : '男',
+#         'age' : '18',
+#     }
+#     return render_template('index.html', **context)
+#
+# @app.route('/login/')
+# def login():
+#     return 'This is login page.'
+#
+# @app.route('/question/<is_login>/')
+# def question(is_login):
+#     if is_login =='1':
+#         return 'This is question page'
+#     else:
+#         login_url = url_for('login')
+#         return redirect(login_url)
+#
+# @app.route('/user/<id>')
+# def get_user(id):
+#     # user = load_user(id)
+#     # if not user:
+#     #     abort(404)
+#     return '<h1>Hello %s!</h1>' % id
+#
+# @app.route('/user/<name>')
+# def user(name):
+#     return '<h1>Hello %s!</h1>' % name
+#
+# if __name__ == '__main__':
+#     manager.run()
+
+
+
+
+"""
+
+    按照章节顺序尝试
+
+"""
+
+from flask import Flask, render_template
+from flask import request, make_response, redirect, abort
+# from flask_script import Manager
 
 
 app = Flask(__name__)
-app.config.from_object(config)
-manager = Manager(app)
+# manager = Manager(app)
 
 @app.route('/')
 def index():
+    # return '<h1>Hello World!</h1>'
+
     # user_agent = request.headers.get('User-Agent')
-    # return '<p>Your Browser is %s</p>' % user_agent
+    # return '<p>Your browser is %s</p>' % user_agent
+
     # response = make_response('<h1>This document carries a cookie!</h1>')
     # response.set_cookie('answer', '42')
     # return response
-    # print (url_for('get_user', id='1'))
-    # print (url_for('user', name='wanglixin'))
-    # return 'Hello world!'
-    # login_url = url_for('login')
-    # return redirect(login_url)
-    context = {
-        'username' : '知了课堂',
-        'gender' : '男',
-        'age' : '18',
-    }
-    return render_template('index.html', **context)
 
-@app.route('/login/')
-def login():
-    return 'This is login page.'
+    # return redirect('http://www.example.com')
 
-@app.route('/question/<is_login>/')
-def question(is_login):
-    if is_login =='1':
-        return 'This is question page'
-    else:
-        login_url = url_for('login')
-        return redirect(login_url)
+    return render_template('index.html')
 
-@app.route('/user/<id>')
-def get_user(id):
-    # user = load_user(id)
-    # if not user:
-    #     abort(404)
-    return '<h1>Hello %s!</h1>' % id
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello %s!</h1>' % name
+    return render_template('user.html', name=name)
+
+# @app.route('/user/<id>')
+# def get_user(id):
+#     user = load_user(id)
+#     if not user:
+#         abort(404)
+#     return '<h1>Hello %s!</h1>' % user.name
+
 
 if __name__ == '__main__':
-    manager.run()
+    app.run(debug=True)
+    # manager.run()
