1a9103214f90466408effadfd2b6323c5261dfa0
wanglixin <1304382389@qq.com>
2018-12-17 16:36:24 +0800

    new files

--- hello.py
+++ hello.py
@@ -65,9 +65,11 @@
 from flask import Flask, render_template
 from flask import request, make_response, redirect, abort
 # from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 # manager = Manager(app)
 
 @app.route('/')
@@ -97,6 +99,14 @@ def user(name):
 #         abort(404)
 #     return '<h1>Hello %s!</h1>' % user.name
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
