5088d994fe91f53c3fba0b6ab0127132493ef8e4
wanglixin <1304382389@qq.com>
2018-12-17 20:50:14 +0800

    changes

--- hello.py
+++ hello.py
@@ -62,17 +62,28 @@
 
 """
 
-from flask import Flask, render_template
-from flask import request, make_response, redirect, abort
+from flask import Flask, render_template, session, redirect, url_for, flash
+# from flask import request, make_response, redirect, abort
 # from flask_script import Manager
 from flask_bootstrap import Bootstrap
-
+from flask_moment import Moment
+from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField, SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 # manager = Manager(app)
 
-@app.route('/')
+class NameForm(Form):
+    name = StringField('What is your name?', validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
     # return '<h1>Hello World!</h1>'
 
@@ -85,7 +96,16 @@ def index():
 
     # return redirect('http://www.example.com')
 
-    return render_template('index.html')
+    # name = None
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+
+    return render_template('index.html', form=form, name=session.get('name'), current_time=datetime.utcnow())
 
 
 @app.route('/user/<name>')
